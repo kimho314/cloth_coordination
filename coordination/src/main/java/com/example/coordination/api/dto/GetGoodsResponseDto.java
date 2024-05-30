48dad891e2ee3e967c3a09bf2585710a871d7a3b
Hoseop.Kim <kimho314@gmail.com>
2024-05-30 22:11:21 +0900

    chore : 사용하지 않는 dto 제거

--- coordination/src/main/java/com/example/coordination/api/dto/GetGoodsResponseDto.java
+++
@@ -1,9 +0,0 @@
-package com.example.coordination.api.dto;
-
-import java.util.List;
-import java.util.Map;
-
-public record GetGoodsResponseDto(
-        Map<String, List<GoodsDto>> goodsMap
-) {
-}
